99a19a8e6f3a9f944ca60f0b70b3f8143c753a8c
fragilebanana16 <850604714@qq.com>
2022-02-27 11:54:15 +0800

    Add simple chase AI

--- BananaManControl.cs
+++ BananaManControl.cs
@@ -18,6 +18,38 @@ public class BananaManControl : MonoBehaviour
     /// Enemy dead
     /// </summary>
     public bool isDead = false;
+
+    /// <summary>
+    /// The player
+    /// </summary>
+    public Transform target;
+
+    /// <summary>
+    /// Player distance with this
+    /// </summary>
+    public float distance;
+
+    /// <summary>
+    /// Look at distance
+    /// </summary>
+    public float lookAtDistance = 25.0f;
+
+    /// <summary>
+    /// Chase distance
+    /// </summary>
+    public float chaseDistance = 15.0f;
+
+    public float damping = 6.0f;
+
+    public CharacterController controler;
+
+    public float gravity = 20f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float moveSpeed = 5.0f;
+
+    public float attackRange = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,14 +61,57 @@ public class BananaManControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isDead)
+        this.distance = Vector3.Distance(target.position, transform.position);
+        if(this.distance < this.lookAtDistance)
         {
-            transform.Translate (Vector3.forward * speed * Time.deltaTime); // move forward
+            anim.SetFloat("speed", 0.1f);
+            this.LookAt();
         }
-        else{
-            transform.Translate (Vector3.zero); // stop movement
+
+
+        if(this.distance > this.lookAtDistance)
+        {
+            anim.SetFloat("speed", 0.1f);
+            transform.Find("Body").GetComponent<SkinnedMeshRenderer>().material.color = Color.green;
         }
+
+        if (this.distance < this.attackRange)
+        {
+            this.Attack();
+        }
+        else if(this.distance < this.chaseDistance)
+        {
+            anim.SetFloat("speed", 0.3f);
+            this.Chase();
+        }
+
+        // ************* Following code translate will pass through object while chasing *********
+        // if (!isDead)
+        // {
+        //     transform.Translate (Vector3.forward * speed * Time.deltaTime); // move forward
+        // }
+        // else{
+        //     transform.Translate (Vector3.zero); // stop movement
+        // }
+        // ****************************************************************************************
     }
 
+    private void LookAt(){
+        transform.Find("Body").GetComponent<SkinnedMeshRenderer>().material.color = Color.yellow;
+        var rotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+    }
 
+    private void Chase(){
+        transform.Find("Body").GetComponent<SkinnedMeshRenderer>().material.color = Color.red;
+        this.moveDirection = transform.forward;
+        this.moveDirection *= moveSpeed;
+
+        this.moveDirection.y -= this.gravity * Time.deltaTime;
+        this.controler.Move(this.moveDirection * Time.deltaTime);
+    }
+
+    private void Attack(){
+
+    }
 }
